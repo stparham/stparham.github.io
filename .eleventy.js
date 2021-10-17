fddc5922dcb3fba9b387dc39be1419311f9f66ac
Stanton Parham <stanton8parham8@gmail.com>
2021-10-17 00:44:16 -0400

    1 Convert Gatsby stuff to 11ty stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy({"src/static": "."})
   eleventyConfig.addPassthroughCopy("src/**/*.css");
 
   return {
     dir: {
       input: "src",
+      includes: "_includes",
+      data: "_data",
       output: "_site"
     }
   };
